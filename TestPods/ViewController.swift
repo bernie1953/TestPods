c05b59a623c5f11029629fea7eeeb309df4ea024
Bernard Maire <b.maire@yahoo.fr>
2019-05-30 14:44:27 +0200

    Initial commit

---
+++ TestPods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPods
+//
+//  Created by Bernard Maire on 30.05.19.
+//  Copyright © 2019 Bernard Maire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
